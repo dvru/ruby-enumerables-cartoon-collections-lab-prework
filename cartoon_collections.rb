6a1497406e0307a8cb0bb4821f706bdff87947b7
Damini Varu <damvaru4@gmail.com>
2019-08-30 23:47:06 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,7 @@ def long_planeteer_calls(array)
   array.any?{ |obj| obj.length > 4}
 end
 
-def find_the_cheesearray)
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   value = array.find_index{ |obj| cheese_types.include?(obj)}
   return array[value] unless value == nil 
